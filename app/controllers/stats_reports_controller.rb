fe2350c087e9e583d7698536b1a0f5c5fb759a5a
ferbin <ferbin@foradian.com>
2020-03-22 20:40:41 +0530

    errors in form fixed

--- app/controllers/stats_reports_controller.rb
+++ app/controllers/stats_reports_controller.rb
@@ -10,7 +10,7 @@ class StatsReportsController < ApplicationController
   end
   
   def create
-    @stats_report = StatsReport.new(permit_params)
+    @stats_report = StatsReport.new(construct_params)
     if @stats_report.save
       redirect_to :controller => "admin_users", :action => "detailed_report"
     else
@@ -19,7 +19,11 @@ class StatsReportsController < ApplicationController
     end
   end
   
-  def permit_params
-    params.require(:stats_report).permit(:date, :district_id, :total_no_patients, :no_of_patients_under_observation, :no_of_patients_obs_at_home, :no_of_patients_obs_at_hospital, :no_of_patients_admitted_today)
-  end
+  protected
+    def construct_params
+      constructed_params = params.require(:stats_report).permit(:date, :district_id, :total_no_patients, :no_of_patients_under_observation, :no_of_patients_obs_at_home, :no_of_patients_obs_at_hospital, :no_of_patients_admitted_today)
+      constructed_params[:no_of_patients_under_observation] = constructed_params[:no_of_patients_obs_at_home].try(:to_i) + constructed_params[:no_of_patients_obs_at_hospital].try(:to_i)
+      constructed_params[:total_no_patients] = constructed_params[:no_of_patients_under_observation].try(:to_i) + constructed_params[:no_of_patients_admitted_today].try(:to_i)
+      constructed_params
+    end
 end
